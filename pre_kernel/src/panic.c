a5783f8f3c7ef788ea7d89c259b43a1924a0f633
Luna :3 <vcvtph2ps@proton.me>
2026-05-28 10:06:16 +1000

    fix: did I even test this code?

--- pre_kernel/src/panic.c
+++ pre_kernel/src/panic.c
@@ -3,7 +3,7 @@
 
 [[noreturn]] void panic(const char* format, ...) {
     va_list args;
-    va_start(args, message);
+    va_start(args, format);
     log_print("prekernel panic: ");
     log_vprint_raw(format, args);
     log_print_raw("\n");
